b34f22d0699d60386395f4f1f8773c3635e73d8d
Rajesh Pillai <pillai.rajesh@gmail.com>
2023-08-31 13:15:57 +0530

    Remix - basics with routing details

---
+++ app/routes/_public+/NOTES.md
@@ -0,0 +1,3 @@
+# This is a pathless route
+
+You can access pages under this using localhost/team, /tos  (and default index.tsx if there is not _index.tsx in the root)
---
+++ remix.config.js
@@ -0,0 +1,29 @@
+import { flatRoutes } from 'remix-flat-routes';
+
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "esm",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  routes: async defineRoutes => {
+		return flatRoutes('routes', defineRoutes, {
+			ignoredRouteFiles: [
+				'.*',
+				'**/*.css',
+				'**/*.test.{js,jsx,ts,tsx}',
+				'**/__*.*',
+			],
+		})
+	},
+};
